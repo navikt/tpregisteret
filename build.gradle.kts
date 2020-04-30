99b50ca6f9fe4c2b9b199e84aee971b1e7208c1b
Mathias Sand Jahren <the.mather1@gmail.com>
2020-04-30 09:47:16 +0200

    Simplify dependencies. Generate working gradle wrapper.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,31 +10,25 @@ plugins {
     kotlin("plugin.spring") version "1.3.72"
     id("org.springframework.boot") version "2.2.6.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    id("com.adarshr.test-logger") version "1.6.0" // pretty log printing
 }
 
 repositories {
     mavenCentral()
 }
 
-
 dependencies {
-    implementation( kotlin("stdlib"))
-    implementation( "org.springframework.boot","spring-boot-starter","2.1.7.RELEASE")
-    implementation( "org.springframework.boot","spring-boot-starter-jdbc","2.1.7.RELEASE")
-    implementation( "org.springframework.boot","spring-boot-starter-web","2.1.7.RELEASE")
-    implementation( "org.springframework.boot","spring-boot-starter-actuator","2.1.7.RELEASE")
-    implementation( "org.springframework.boot","spring-boot-starter-oauth2-resource-server","2.1.7.RELEASE")
+    implementation(kotlin("reflect"))
+    implementation( "org.springframework.boot","spring-boot-starter-web")
+    implementation( "org.springframework.boot","spring-boot-starter-jdbc")
+    implementation( "org.springframework.boot","spring-boot-starter-actuator")
+    implementation( "org.springframework.boot","spring-boot-starter-oauth2-resource-server")
     implementation( "org.springframework.security.oauth","spring-security-oauth2","2.3.6.RELEASE")
     implementation( "com.oracle.ojdbc","ojdbc8","19.3.0.0")
     implementation( "io.micrometer","micrometer-registry-prometheus","1.1.5")
-    implementation( "ch.qos.logback","logback-classic","1.2.3")
     implementation( "net.logstash.logback","logstash-logback-encoder","6.2")
-    implementation( "javax.xml.bind","jaxb-api","2.3.0")
     implementation( "com.sun.xml.bind","jaxb-impl","2.3.0")
     implementation( "org.glassfish.jaxb","jaxb-runtime","2.3.0")
-    implementation( "javax.activation","activation","1.1.1")
-    testImplementation( "org.jetbrains.kotlin","kotlin-test-junit5","1.3.50")
+    testImplementation(kotlin("test-junit5"))
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
@@ -42,12 +36,11 @@ dependencies {
     testImplementation( "com.github.tomakehurst","wiremock","2.23.2")
 }
 
-
 tasks{
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "13"
     }
-    withType<Test> {
+    test{
         useJUnitPlatform()
     }
 }
