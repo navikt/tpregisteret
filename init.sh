7ef0f2f4c3774226e7b8769a9e5448599cc11e52
Magnus Austrheim <Magnus.Austrheim@nav.no>
2020-08-12 09:19:18 +0200

    Inject jdbc url from vault

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-export SPRING_DATASOURCE_USERNAME=$(cat /secrets/oracle/username)
-export SPRING_DATASOURCE_PASSWORD=$(cat /secrets/oracle/password)
+export SPRING_DATASOURCE_URL=$(cat /secrets/oracle/config/jdbc_url)
+export SPRING_DATASOURCE_USERNAME=$(cat /secrets/oracle/creds/username)
+export SPRING_DATASOURCE_PASSWORD=$(cat /secrets/oracle/creds/password)
