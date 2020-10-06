e0442ebba2c6b01703f094eaa98f23df3cd9821c
Mathias Sand Jahren <Mathias.Sand.Jahren@nav.no>
2020-10-06 09:41:47 +0200

    Move EnableJpaRepositories annotation to application class.

--- src/main/kotlin/no/nav/tpregisteret/config/SecurityConfig.kt
+++ src/main/kotlin/no/nav/tpregisteret/config/SecurityConfig.kt
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Profile
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @Configuration
-@EnableJpaRepositories
 @EnableJwtTokenValidation
 @Profile("!test")
 class SecurityConfig
