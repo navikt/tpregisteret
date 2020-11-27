b5c6abecaf132339a6f92d5eebff284018209d33
Mathias Sand Jahren <Mathias.Sand.Jahren@nav.no>
2020-11-27 12:56:14 +0100

    Improve exception handling.

--- src/main/kotlin/no/nav/tpregisteret/exceptions/ExceptionHandler.kt
+++ src/main/kotlin/no/nav/tpregisteret/exceptions/ExceptionHandler.kt
@@ -3,11 +3,13 @@ package no.nav.tpregisteret.exceptions
 import io.prometheus.client.Counter
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.core.annotation.AnnotationUtils.findAnnotation
 import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.http.HttpStatus.*
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.server.ResponseStatusException
 
 
 @ControllerAdvice
@@ -25,8 +27,13 @@ class ExceptionHandler {
 
     @ExceptionHandler(Throwable::class)
     @ResponseStatus(INTERNAL_SERVER_ERROR)
-    fun internalServerError(e : Throwable): String? {
-        if (e is EmptyResultDataAccessException || e is NotImplementedError || e is ResursIkkeFunnet) throw e
+    fun internalServerError(e: Throwable): String? {
+        if (
+                e is EmptyResultDataAccessException
+                || e is NotImplementedError
+                || e is ResponseStatusException
+                || findAnnotation(e::class.java, ResponseStatus::class.java) != null
+        ) throw e
         LOG.error("Something went wrong.", e)
         errorCounter.labels(e::class.simpleName).inc()
         return e.message
@@ -35,9 +42,11 @@ class ExceptionHandler {
 
     @ExceptionHandler(EmptyResultDataAccessException::class)
     @ResponseStatus(NOT_FOUND)
-    fun emptyResultFromRepository(e : EmptyResultDataAccessException) {/*No body*/}
+    fun emptyResultFromRepository(e: EmptyResultDataAccessException) {/*No body*/
+    }
 
     @ExceptionHandler(NotImplementedError::class)
     @ResponseStatus(NOT_IMPLEMENTED)
-    fun notImplemented(e : NotImplementedError) {/*No body*/}
+    fun notImplemented(e: NotImplementedError) {/*No body*/
+    }
 }
