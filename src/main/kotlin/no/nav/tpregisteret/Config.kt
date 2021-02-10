86b438f44c353dcf8aeb3dbf6c39e325650c39e6
TheMather1 <the.mather1@gmail.com>
2021-02-10 17:24:15 +0100

    Add @EnableMaskinportenValidation

---
+++ src/main/kotlin/no/nav/tpregisteret/Config.kt
@@ -0,0 +1,10 @@
+package no.nav.tpregisteret
+
+import no.nav.pensjonsamhandling.maskinporten.validation.annotation.EnableMaskinportenValidation
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+@Configuration
+@Profile("!test")
+@EnableMaskinportenValidation
+class Config
